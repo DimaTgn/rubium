ee012d61e29f78cfc46befe98b9340372e22a432
DimaTgn <dmitriy_demyanenko@mail.ru>
2020-08-15 02:53:58 +0300

    burger-menu, start mobile version

--- src/js/burger.js
+++ src/js/burger.js
@@ -1,13 +1,17 @@
-let onMobileMenu = false;
-const elemMenu = document.getElementsByClassName("burger-menu__box");
-function menuOn(x) {
-  if(onMobileMenu === false){
-    elemMenu[0].style.visibility = "visible";
-    onMobileMenu = true;
-  }
-  else{
-    elemMenu[0].style.visibility = "hidden";
-    onMobileMenu = false;
-  }
-  x.classList.toggle("change");
+document.addEventListener("DOMContentLoaded", ready);
+function ready(){
+  let onMobileMenu = false;
+  const elemMenu = document.getElementsByClassName("burger-menu__box");
+  const elemBurger = document.getElementsByClassName("nav__menu");
+  elemBurger[0].addEventListener("click", function(){
+    if(onMobileMenu === false){
+      elemMenu[0].style.visibility = "visible";
+      onMobileMenu = true;
+    }
+    else{
+      elemMenu[0].style.visibility = "hidden";
+      onMobileMenu = false;
+    }
+    this.classList.toggle("change");
+  })
 }
