42e3889926e03206553b4aec5447bf06d3ee48a5
DimaTgn <dmitriy_demyanenko@mail.ru>
2020-08-18 03:09:16 +0300

    Mobile version

--- src/js/accordeon.js
+++ src/js/accordeon.js
@@ -1,9 +1,24 @@
 $(document).ready(function() {
-  $('.class_a').on('click', f_acc);
+  if($(document).width() < 769 ){
+    $('.class_a').on('click', f_acc);
+  }
+  else{
+    $('.class_a').off('click', f_acc);
+  }
 });
 
 function f_acc(){
   $('.class_b').not($(this).next()).slideUp(200);
   $(this).next().slideToggle(200);
 }
-console.log("123")
+$(window).resize(function() {
+  let width = 0;
+  width = $(window).width();
+  if (width < 769) {
+    $('.class_a').off('click', f_acc);
+    $('.class_a').on('click', f_acc);
+  }
+  else{
+    $('.class_a').off('click', f_acc);
+  }
+});
